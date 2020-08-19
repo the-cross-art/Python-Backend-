b80031e8fc421085726fa3d6e02666e662dd8876
the-cross-art <imrannaz326@gmail.com>
2020-08-19 20:55:36 +0530

    Calculator with Python DONE!!

--- calc.py
+++ calc.py
@@ -1,6 +1,32 @@
-# check = False
+import re
 
-# if check==False:
-#     print("It is false")
-# else:
-#    print("It is actually true") 
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n")
+print("-"*40)
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous==0:
+        equation = input("Enter equation:")
+    
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+         print("GoodBye!! Thanks on using Calc")
+         run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()""]','',equation)
+        
+        if previous==0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous)+equation)
+
+while run:
+    performMath()
