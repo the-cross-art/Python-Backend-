0b3b70690d46428c132399e270f88fd3d2e9cef1
the-cross-art <imrannaz326@gmail.com>
2020-08-21 23:36:38 +0530

    PEP start

---
+++ main_pep.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+
+def my_functions(one,two,three,four,five,six):
+    print("Hello world")
+
+def second_finctions():
+    print("Second functions")
+
+my_list = [1,2,
+            3,4
+            5,6]
+
+print("Hello","World")
+
+x=3
